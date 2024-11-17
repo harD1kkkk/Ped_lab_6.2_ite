98713bc87bb4b3c1abd7132f11760c8163fa61f7
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-11-17 18:19:34 +0200

    Add files via upload

---
+++ LAB_6.2_ite/LAB_6.2_ite/lab_6.2_ite.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+void CreateArrayWithRandomElements(int* a, int numberOfElements, int start, int end) {
+    for (int i = 0; i < numberOfElements; i++) {
+        a[i] = start + rand() % (end - start + 1);
+    }
+}
+
+void PrintArray(int* a, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "a[" << i << "] = " << a[i] << "; ";
+    }
+    cout << endl;
+}
+
+int FindSumOfMaxAndMin(int* a, int size) {
+    int maxElement = a[0];
+    int minElement = a[0];
+
+    for (int i = 1; i < size; i++) {
+        if (a[i] > maxElement) {
+            maxElement = a[i];
+        }
+        if (a[i] < minElement) {
+            minElement = a[i];
+        }
+    }
+
+    return maxElement + minElement;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    const int size = 10;
+    int array[size];
+
+    CreateArrayWithRandomElements(array, size, -50, 50);
+
+    cout << "Array:" << endl;
+    PrintArray(array, size);
+
+    int sum = FindSumOfMaxAndMin(array, size);
+
+    cout << "\nSum of max and min elements: " << sum << endl;
+    return 0;
+}
